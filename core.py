721bb2bd59493993799445f98c5495991116c57c
badvvolf <>
2020-08-20 22:20:42 +0900

    18장 리팩토링 - 일반화

--- core.py
+++ core.py
@@ -1,11 +1,15 @@
-class WasRun(object):
+class TestCase(object):
     def __init__(self, name):
-        self.wasRun = None
         self.name = name
-    
-    def testMethod(self):
-        self.wasRun = 1
-    
+
     def run(self):
         method = getattr(self, self.name)
         method()
+
+class WasRun(TestCase):
+    def __init__(self, name):
+        self.wasRun = None
+        super().__init__(name)
+    
+    def testMethod(self):
+        self.wasRun = 1
