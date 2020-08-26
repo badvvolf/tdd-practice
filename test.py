8fb17598b8c5272a08fe5b5f20d819ba5a7fcb4a
badvvolf <>
2020-08-26 19:06:35 +0900

    22장 테스트 추가

--- test.py
+++ test.py
@@ -1,4 +1,4 @@
-from core import TestCase, WasRun
+from core import TestCase, WasRun, TestResult
 
 class TestCaseTest(TestCase):
     def testTemplateMethod(self):
@@ -16,6 +16,13 @@ class TestCaseTest(TestCase):
         result = test.run()
         assert("1 run, 1 failed" == result.summary())
 
+    def testFailedResultFormatting(self):
+        result = TestResult()
+        result.testStarted()
+        result.testFailed()
+        assert("1 run, 1 failed" == result.summary())
+
 TestCaseTest("testTemplateMethod").run()
 TestCaseTest("testResult").run()
 TestCaseTest("testFailedResult").run()
+TestCaseTest("testFailedResultFormatting").run()
