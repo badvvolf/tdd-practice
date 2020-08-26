ecc8ef8c6ddc4a1c2ee35f9ddf5d3f91771c1eec
badvvolf <>
2020-08-26 18:13:13 +0900

    21장 리팩토링

--- core.py
+++ core.py
@@ -6,18 +6,24 @@ class TestCase(object):
         pass
 
     def run(self):
+        result = TestResult()
+        result.testStarted()
         self.setUp()
         method = getattr(self, self.name)
         method()
         self.tearDown()
-        return TestResult()
+        return result
 
     def tearDown(self):
         pass
 
 class TestResult(object):
+    def __init__(self):
+        self.runCount = 0
+    def testStarted(self):
+        self.runCount = self.runCount + 1
     def summary(self):
-        return "1 run, 0 failed"
+        return "{} run, 0 failed".format(self.runCount)
 
 class WasRun(TestCase):
     def __init__(self, name):
